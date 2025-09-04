361b91e2c148913e586a814b00e80dc707866aed
Rizuca <qiqiakun1@gmail.com>
2025-09-04 15:21:23 +0700

    info

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ export default function App() {
         <div className="text-center space-y-2">
           <h1>Queue Data Structure Implementation</h1><br>
             <p className="text-muted-foreground">
-            Implementasi Queue dengan Array Statis, Linked List, dan Circular Queue
+            Implementasi Queue dengan Array Statis, Linked List, dan Circular Queue dari kelompok 7 yang beranggotakan : <br />
+            Rifqi, Nazwa, Repi, Nuriana
           </p>
         </div>
 
